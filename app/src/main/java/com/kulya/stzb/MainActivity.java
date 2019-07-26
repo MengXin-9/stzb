35d4005e3e5419a78e2a5f3c56325bab9f28c882
kulya91 <1942186814@qq.com>
2019-07-26 12:26:05 +0800

    显示时间

--- app/src/main/java/com/kulya/stzb/MainActivity.java
+++ app/src/main/java/com/kulya/stzb/MainActivity.java
@@ -1,13 +1,7 @@
 package com.kulya.stzb;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
--- app/src/main/java/com/kulya/stzb/WindowService.java
+++ app/src/main/java/com/kulya/stzb/WindowService.java
@@ -1,5 +1,6 @@
 package com.kulya.stzb;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -9,14 +10,18 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.SystemClock;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -31,44 +36,62 @@ public class WindowService extends Service {
     WindowManager.LayoutParams params;
     //实例化的WindowManager.
     WindowManager windowManager;
-    ImageButton imageButton1;
+    Button imageButton1;
     //状态栏高度.（接下来会用到）
     int statusBarHeight = -1;
 
     public WindowService() {
     }
 
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    long sysTime = System.currentTimeMillis();
+                    CharSequence sysTimeStr = DateFormat.format("hh:mm:ss", sysTime);
+                    imageButton1.setText(sysTimeStr); //更新时
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
-//        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            NotificationChannel notificationChannel = new NotificationChannel("default", "name", NotificationManager.IMPORTANCE_HIGH);
-//            manager.createNotificationChannel(notificationChannel);
-//        }
-//        Notification notification = new NotificationCompat.Builder(this, "default")
-//                .setContentTitle("我是标题！")//设置标题
-//                .setContentText("我是内容")//设置内容
-//                .setWhen(System.currentTimeMillis())//设置显示通知被创建的时间
-//                .setSmallIcon(R.mipmap.ic_launcher)//设置通知的小图标
-//                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))//设置通知的大图标
-//                .setAutoCancel(true)//通知自动取消
-//                .build();
-//        //显示通知，每条通知的id都要不同
-//        startForeground(1, notification);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("default", "name", NotificationManager.IMPORTANCE_HIGH);
+            manager.createNotificationChannel(notificationChannel);
+        }
+        Notification notification = new NotificationCompat.Builder(this, "default")
+                .setContentTitle("前台服务")//设置标题
+                .setContentText("率土助手正在运行")//设置内容
+                .setWhen(System.currentTimeMillis())//设置显示通知被创建的时间
+                .setSmallIcon(R.mipmap.ic_launcher)//设置通知的小图标
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))//设置通知的大图标
+                .setAutoCancel(true)//通知自动取消
+                .build();
+        //显示通知，每条通知的id都要不同
+        startForeground(1, notification);
         createToucher();
+        new TimeThread().start();
     }
-    private void createToucher()
-    {
+
+    private void createToucher() {
         //赋值WindowManager&LayoutParam.
         params = new WindowManager.LayoutParams();
         windowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
         //设置type.系统提示型窗口，一般都在应用程序窗口之上.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-    }else {
+        } else {
             params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
-    }
+        }
 
         //设置效果为背景透明.
         params.format = PixelFormat.RGBA_8888;
@@ -89,51 +112,32 @@ public class WindowService extends Service {
 
         LayoutInflater inflater = LayoutInflater.from(getApplication());
         //获取浮动窗口视图所在布局.
-        toucherLayout = (ConstraintLayout) inflater.inflate(R.layout.window,null);
+        toucherLayout = (ConstraintLayout) inflater.inflate(R.layout.window, null);
 //        //添加toucherlayout
         try {
-            windowManager.addView(toucherLayout,params);
+            windowManager.addView(toucherLayout, params);
+        } catch (Exception e) {
+
         }
-       catch (Exception e){
 
-       }
-
-        Log.i(TAG,"toucherlayout-->left:" + toucherLayout.getLeft());
-        Log.i(TAG,"toucherlayout-->right:" + toucherLayout.getRight());
-        Log.i(TAG,"toucherlayout-->top:" + toucherLayout.getTop());
-        Log.i(TAG,"toucherlayout-->bottom:" + toucherLayout.getBottom());
+        Log.i(TAG, "toucherlayout-->left:" + toucherLayout.getLeft());
+        Log.i(TAG, "toucherlayout-->right:" + toucherLayout.getRight());
+        Log.i(TAG, "toucherlayout-->top:" + toucherLayout.getTop());
+        Log.i(TAG, "toucherlayout-->bottom:" + toucherLayout.getBottom());
 
         //主动计算出当前View的宽高信息.
-        toucherLayout.measure(View.MeasureSpec.UNSPECIFIED,View.MeasureSpec.UNSPECIFIED);
+        toucherLayout.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
 
         //用于检测状态栏高度.
-        int resourceId = getResources().getIdentifier("status_bar_height","dimen","android");
-        if (resourceId > 0)
-        {
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
             statusBarHeight = getResources().getDimensionPixelSize(resourceId);
         }
-        Log.i(TAG,"状态栏高度为:" + statusBarHeight);
+        Log.i(TAG, "状态栏高度为:" + statusBarHeight);
 
         //浮动窗口按钮.
-        imageButton1 = (ImageButton) toucherLayout.findViewById(R.id.imageButton1);
-        imageButton1.setOnClickListener(new View.OnClickListener() {
+        imageButton1 =  toucherLayout.findViewById(R.id.imageButton1);
             long[] hints = new long[2];
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG,"点击了");
-                System.arraycopy(hints,1,hints,0,hints.length -1);
-                hints[hints.length -1] = SystemClock.uptimeMillis();
-                if (SystemClock.uptimeMillis() - hints[0] >= 700)
-                {
-                    Log.i(TAG,"要执行");
-                    Toast.makeText(WindowService.this,"连续点击两次以退出",Toast.LENGTH_SHORT).show();
-                }else
-                {
-                    Log.i(TAG,"即将关闭");
-                    stopSelf();
-                }
-            }
-        });
         imageButton1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -141,19 +145,18 @@ public class WindowService extends Service {
                 params.x = (int) event.getRawX() - 150;
                 //这就是状态栏偏移量用的地方
                 params.y = (int) event.getRawY() - 150 - statusBarHeight;
-                windowManager.updateViewLayout(toucherLayout,params);
+                windowManager.updateViewLayout(toucherLayout, params);
                 return false;
             }
         });
 
         //其他代码...
     }
+
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         //用imageButton检查悬浮窗还在不在，这里可以不要。优化悬浮窗时要用到。
-        if (imageButton1 != null)
-        {
+        if (imageButton1 != null) {
             windowManager.removeView(toucherLayout);
         }
         super.onDestroy();
@@ -163,6 +166,22 @@ public class WindowService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         return null;
-       // throw new UnsupportedOperationException("Not yet implemented");
+        // throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    class TimeThread extends Thread {
+        @Override
+        public void run() {
+            do {
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;  //消息(一个整型值)
+                    mHandler.sendMessage(msg);// 每隔1秒发送一个msg给mHandler
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } while (true);
+        }
     }
 }
